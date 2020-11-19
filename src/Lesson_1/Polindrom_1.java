925006365faf69b9476cfa433e34a5c32b883f49
MarihaF92 <ms.marina_f@mail.ru>
2020-11-19 12:29:23 +0300

    new task

---
+++ src/Lesson_1/Polindrom_1.java
@@ -0,0 +1,32 @@
+package Lesson_1;
+
+import java.util.Scanner;
+/*
+Необходимо проверить, является ли введенное число, состоящее из 5 цифр полиндромом
+Если введено более 5 или менее 5 цифр - ожидать введения нового числа
+*/
+
+public class Polindrom_1 {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        boolean flag = true;
+        while (flag) {
+            System.out.println("Enter your number");
+            int number = in.nextInt();
+            if ((number <= 99999) & (number >= 9999)) {
+                flag = false;
+                int a = number % 10;
+                int b = (number/10) % 10;
+                int c = (number/100) % 10;
+                int d = (number/1000) % 10;
+                int e = (number/10000) % 10;
+                if ((a == e) & (b == d)) {
+                    System.out.println("Number is polindrom!");
+                } else {
+                    System.out.println("Number is not polindrom!");
+                }
+            }
+        }
+    }
+}
